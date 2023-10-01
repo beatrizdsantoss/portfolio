b72c0a90eb3dd5dcd576b67037248854a62925f4
beatrizdsantoss <beatrizdsantos1250@gmail.com>
2023-09-30 17:17:24 -0700

    update

--- contactinputs.php
+++ contactinputs.php
@@ -1,6 +1,13 @@
 <?php
-$username = $_POST['name'];
-$email = $_POST['email'];
-$object = $_POST['object'];
-$message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $object = $_POST["object"];
+    $message = $_POST["message"];
+
+    // Your processing logic here...
+
+    // For debugging, you can use var_dump or print_r to check the received data
+    var_dump($_POST);
+}
 ?>
